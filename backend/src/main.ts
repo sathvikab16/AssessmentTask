576204914189ef3e8ae96218374c4091f08c6acb
sathvikab16 <sathvikab16@gmail.com>
2026-08-13 20:28:51 +0530

    Enable CORS for live frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 
 bootstrap();
